0cfa7f74d5273bffc17c87a571d6b0ca7689b3dd
crialarc <cristianalarcon@gmail.com>
2016-08-26 00:16:15 -0300

    fix font

--- farmacia-popular/src/main/java/cl/nullpointer/farmaciapopular/main/Main.java
+++ farmacia-popular/src/main/java/cl/nullpointer/farmaciapopular/main/Main.java
@@ -5,8 +5,10 @@ import base.utilidades.Utils;
 import cl.nullpointer.farmaciapopular.paneles.PanelPrincipal;
 import com.pagosoft.plaf.PlafOptions;
 import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 import org.apache.log4j.Logger;
 
 /**
@@ -33,6 +35,8 @@ public class Main {
         // Establecer apariencia de la aplicación
         PlafOptions.setAsLookAndFeel();
         PlafOptions.updateAllUIs();
+
+        setUIFont(new javax.swing.plaf.FontUIResource("UnDotum", Font.PLAIN, 13));
 
         // Comprobar que la aplicación no se encuentra corriendo
         if (!Utils.comprobarInstancia()) {
@@ -61,5 +65,16 @@ public class Main {
         ventanaPrincipal.setLocationRelativeTo(null);
         ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventanaPrincipal.setVisible(true);
+    }
+
+    public static void setUIFont(javax.swing.plaf.FontUIResource f) {
+        java.util.Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value != null && value instanceof javax.swing.plaf.FontUIResource) {
+                UIManager.put(key, f);
+            }
+        }
     }
 }
